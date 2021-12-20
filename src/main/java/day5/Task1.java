00806fe25e2dd59365788baa8eaf3ce1b1c6b1b1
Garoma22 <garomait@gmail.com>
2021-12-20 17:01:25 +0300

    added day 5

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,40 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car car = new Car();
+        car.setModel("Ford Ranger");
+        car.setColor("Blue");
+        car.setRelease(2005);
+        System.out.println(car.getModel());
+        System.out.println(car.getColor());
+        System.out.println(car.getRelease());
+    }
+}
+class Car{
+    private String model;
+    private String color;
+    private int release;
 
+    public void setModel(String model) {
+
+        this.model = model;
+    }
+    public void setColor(String color){
+        if (color.isEmpty())
+            System.out.println("Хамелион");
+        this.color = color;
+    }
+    public void setRelease(int reliase){
+        if(reliase < 2000)
+            System.out.println("Машина времени");
+        else
+        this.release = reliase;
+    }
+    public String getModel() {
+        return model;}
+    public String getColor(){
+        return color;}
+    public int getRelease (){
+        return release;
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,40 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike enduro = new Motorbike("BMW", "Black",1997);
+        System.out.println(enduro.getModel());
+        System.out.println(enduro.getColor());
+        System.out.println(enduro.getRelease());
 
+        System.out.println();
+
+        Motorbike enduro1 = new Motorbike("Jawa","Red");
+        System.out.println(enduro1.getModel());
+        System.out.println(enduro1.getColor());
+    }
+}
+class Motorbike{
+
+        private String model;
+        private String color ;
+        private int release;
+
+        public Motorbike (String model, String color){
+            this.model = model;
+            this.color = color;
+        }
+        public Motorbike(String model, String color, int release){
+            this.model = model;
+            this.color = color;
+            this.release = release;
+        }
+        public String getModel(){
+            return model;
+        }
+        public String getColor(){
+            return color;
+        }
+        public int getRelease() {
+        return release;
     }
 }
